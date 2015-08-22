1b9c324305035c24f083db843ae8f5eeafb5342a
rpeschke <richard.peschke@desy.de>
2015-08-22 21:21:53 +0200

    conversion from local to global is finally working. matrix transform not working

--- sct_plot/inc/geometry/setup_description.hh
+++ sct_plot/inc/geometry/setup_description.hh
@@ -6,6 +6,7 @@
 #include "rapidxml_utils.hpp"
 #include "rapidxml.hpp"
 #include "xml_helpers/xml_util.hh"
+#include "TMath.h"
 #define  sct_corr_XML_ERROR_DEFAULT_VALUE  -666666666666
 namespace sct_corr {
 using xml_n = rapidxml::xml_node < char >;
@@ -125,8 +126,8 @@ Matrix_3x3 RotZ(Double_t phix) {
 
 struct Xladder {
 public:
-  Xladder(xml_n* n):
-  
+  Xladder(xml_n* n) :
+
     ID(xml_util::getAtribute(n, "ID", ID)),
     positionX(xml_util::getAtribute(n, "positionX", positionX)),
     positionY(xml_util::getAtribute(n, "positionY", positionY)),
@@ -139,18 +140,18 @@ public:
 
     sizeX(xml_util::getAtribute(n, "sizeX", sizeX)),
     sizeY(xml_util::getAtribute(n, "sizeY", sizeY)),
-    thickness(xml_util::getAtribute(n, "thickness", thickness))
-  {}
-  double ID= sct_corr_XML_ERROR_DEFAULT_VALUE,
-    positionX= sct_corr_XML_ERROR_DEFAULT_VALUE,
-    positionY= sct_corr_XML_ERROR_DEFAULT_VALUE,
-    positionZ= sct_corr_XML_ERROR_DEFAULT_VALUE,
-    radLength= sct_corr_XML_ERROR_DEFAULT_VALUE,
-    rotationXY= sct_corr_XML_ERROR_DEFAULT_VALUE,
-    rotationZX= sct_corr_XML_ERROR_DEFAULT_VALUE,
-    rotationZY= sct_corr_XML_ERROR_DEFAULT_VALUE,
-    sizeX= sct_corr_XML_ERROR_DEFAULT_VALUE,
-    sizeY= sct_corr_XML_ERROR_DEFAULT_VALUE,
+    thickness(xml_util::getAtribute(n, "thickness", thickness)) {
+  }
+  double ID = sct_corr_XML_ERROR_DEFAULT_VALUE,
+    positionX = sct_corr_XML_ERROR_DEFAULT_VALUE,
+    positionY = sct_corr_XML_ERROR_DEFAULT_VALUE,
+    positionZ = sct_corr_XML_ERROR_DEFAULT_VALUE,
+    radLength = sct_corr_XML_ERROR_DEFAULT_VALUE,
+    rotationXY = sct_corr_XML_ERROR_DEFAULT_VALUE,
+    rotationZX = sct_corr_XML_ERROR_DEFAULT_VALUE,
+    rotationZY = sct_corr_XML_ERROR_DEFAULT_VALUE,
+    sizeX = sct_corr_XML_ERROR_DEFAULT_VALUE,
+    sizeY = sct_corr_XML_ERROR_DEFAULT_VALUE,
     thickness = sct_corr_XML_ERROR_DEFAULT_VALUE;
 
 
@@ -159,7 +160,7 @@ struct Xsensitive {
 public:
   Xsensitive(xml_n* n) :
 
-    ID ( xml_util::getAtribute(n, "ID", ID)),
+    ID(xml_util::getAtribute(n, "ID", ID)),
     npixelX(xml_util::getAtribute(n, "npixelX", npixelX)),
     npixelY(xml_util::getAtribute(n, "npixelY", npixelY)),
 
@@ -182,29 +183,30 @@ public:
     sizeY(xml_util::getAtribute(n, "sizeY", sizeY)),
     thickness(xml_util::getAtribute(n, "thickness", thickness))
 
-  {}
+  {
+  }
   double ID = sct_corr_XML_ERROR_DEFAULT_VALUE,
-  npixelX = sct_corr_XML_ERROR_DEFAULT_VALUE,
-    npixelY= sct_corr_XML_ERROR_DEFAULT_VALUE,
-    pitchX= sct_corr_XML_ERROR_DEFAULT_VALUE,
-    pitchY= sct_corr_XML_ERROR_DEFAULT_VALUE,
-    positionX= sct_corr_XML_ERROR_DEFAULT_VALUE,
-    positionY= sct_corr_XML_ERROR_DEFAULT_VALUE,
-    positionZ= sct_corr_XML_ERROR_DEFAULT_VALUE,
-    radLength= sct_corr_XML_ERROR_DEFAULT_VALUE,
-    resolution= sct_corr_XML_ERROR_DEFAULT_VALUE,
-    rotation1= sct_corr_XML_ERROR_DEFAULT_VALUE,
-    rotation2= sct_corr_XML_ERROR_DEFAULT_VALUE,
-    rotation3= sct_corr_XML_ERROR_DEFAULT_VALUE,
-    rotation4= sct_corr_XML_ERROR_DEFAULT_VALUE,
-    sizeX= sct_corr_XML_ERROR_DEFAULT_VALUE,
-    sizeY= sct_corr_XML_ERROR_DEFAULT_VALUE,
+    npixelX = sct_corr_XML_ERROR_DEFAULT_VALUE,
+    npixelY = sct_corr_XML_ERROR_DEFAULT_VALUE,
+    pitchX = sct_corr_XML_ERROR_DEFAULT_VALUE,
+    pitchY = sct_corr_XML_ERROR_DEFAULT_VALUE,
+    positionX = sct_corr_XML_ERROR_DEFAULT_VALUE,
+    positionY = sct_corr_XML_ERROR_DEFAULT_VALUE,
+    positionZ = sct_corr_XML_ERROR_DEFAULT_VALUE,
+    radLength = sct_corr_XML_ERROR_DEFAULT_VALUE,
+    resolution = sct_corr_XML_ERROR_DEFAULT_VALUE,
+    rotation1 = sct_corr_XML_ERROR_DEFAULT_VALUE,
+    rotation2 = sct_corr_XML_ERROR_DEFAULT_VALUE,
+    rotation3 = sct_corr_XML_ERROR_DEFAULT_VALUE,
+    rotation4 = sct_corr_XML_ERROR_DEFAULT_VALUE,
+    sizeX = sct_corr_XML_ERROR_DEFAULT_VALUE,
+    sizeY = sct_corr_XML_ERROR_DEFAULT_VALUE,
     thickness = sct_corr_XML_ERROR_DEFAULT_VALUE;
 
 };
 struct Xlayer {
 public:
-  Xlayer(xml_n* n) :ladder(n->first_node("ladder")), sensitive(n->first_node("sensitive")){}
+  Xlayer(xml_n* n) :ladder(n->first_node("ladder")), sensitive(n->first_node("sensitive")) {}
   Xladder ladder;
   Xsensitive  sensitive;
   sct_corr::linear_algebra::xyz_hit getTranslation() const {
@@ -251,7 +253,23 @@ public:
   std::string geartype, name;
   double siplanesID = 0;
   std::string siplanesType;
-  double siplanesNumber=0;
+  double siplanesNumber = 0;
+  Xlayer* layer_by_ID(double ID) {
+    for (auto& e : layer) {
+      if (e.ladder.ID == ID) {
+        return &e;
+      }
+    }
+    return nullptr;
+  }
+  const Xlayer* layer_by_ID(double ID) const {
+    for (auto& e : layer) {
+      if (e.ladder.ID == ID) {
+        return &e;
+      }
+    }
+    return nullptr;
+  }
   std::vector<Xlayer> layer;
 };
 struct XBField {
@@ -264,7 +282,7 @@ public:
 struct Xgear {
 
 public:
-  Xgear(xml_n* n) :detectorName(xml_util::getAtribute(n->first_node("global"), "detectorName", "notSet")), bfield(n->first_node("BField")), detector(n->first_node("detectors")->first_node("detector")){}
+  Xgear(xml_n* n) :detectorName(xml_util::getAtribute(n->first_node("global"), "detectorName", "notSet")), bfield(n->first_node("BField")), detector(n->first_node("detectors")->first_node("detector")) {}
 
 
   std::string detectorName;
